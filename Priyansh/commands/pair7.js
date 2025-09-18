2cf0a3a5c6547665047baeebef42c941ceb87e48
Allux3s41m <alluxtylish000@gmail.com>
2025-09-18 23:01:50 +0300

    Update pair7.js

--- Priyansh/commands/pair7.js
+++ Priyansh/commands/pair7.js
@@ -46,8 +46,8 @@ async function makeImage({ one, two }) {
     let circleTwo = await jimp.read(await circle(avatarTwo));
 
     // ==== DP SIZE & POSITION FIX ====
-    let dpWidth = 400;
-    let dpHeight = 403;
+    let dpWidth = 200;
+    let dpHeight = 205;
 
     // Adjust positions according to frame for perfect alignment
     let dpOneX = 350; // sender DP X
